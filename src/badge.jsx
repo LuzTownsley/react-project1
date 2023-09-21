bb24de86e226c66f50879044b3472d25cfd66b0a
Luz Townsley <luz@Luzs-MacBook-Pro.local>
2023-09-20 21:16:40 -0400

    Quiz finalizado de react-project1

---
+++ src/badge.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./badge.css";
+
+function Badge({ label }) {
+  return (
+    <span className="badge">
+      {label}
+    </span>
+  );
+}
+
+export default Badge;
